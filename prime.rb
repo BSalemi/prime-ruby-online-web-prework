585ee0d72286710b8b01a45922c2a49421ba6bc5
Brian Salemi <briandsalemi@gmail.com>
2019-04-23 15:52:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(n)
   return false if !n.integer?
   return false if n < 2
   return true if n == 2
-  m = (2..n-1)
+  m = (2..Math.sqrt(n))
   m_range = m.to_a
   m_range.each {|number| return false if n % number == 0}
   true
