6ca6361d4fcb1eb0a6401ebcccd201f1535b3605
Brian Salemi <briandsalemi@gmail.com>
2019-04-23 15:21:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
-
+require 'benchmark'
 
 def prime?(n)
-  puts Benchmark.measure
-  {return false if !n.integer?
+  return false if !n.integer?
   return false if n < 2
   return true if n == 2
   m = (2..Math.sqrt(n))
   m_range = m.to_a
-  m_range.each {|number| return false if n % number == 0}
-  true}
+  puts Benchmark.measure {m_range.each {|number| return false if n % number == 0}}
+  true
 end
